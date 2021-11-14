d9f07b04d874fd1d80a97c2fc624b7cac17945cc
bepalacio <30908023+bepalacio@users.noreply.github.com>
2021-11-13 21:05:11 -0500

    Tarjetas de paises

--- apps/front-final/app/countries/countries.tsx
+++ apps/front-final/app/countries/countries.tsx
@@ -1,4 +1,8 @@
 import './countries.module.css';
+import Link from "next/link";
+import imagenDefault from "../../public/Images/fondoSabanaFutbol.jpg";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Image from 'next/image'
 import {useCountries,testApi} from '../useCountries'
 
 /* eslint-disable-next-line */
@@ -17,7 +21,28 @@ export function Countries(props: CountriesProps) {
       {
         countries && countries.length > 0 
         ?
-        countries.map(({country_id, name,country_code}) =>  country_code && <li key={country_id}>{country_id} - {name}</li>)
+        countries.map(({country_id, name, country_code, continent}) =>  country_code &&
+          <div className="container mb-4">
+            <div className="card">
+              <Image 
+                src={imagenDefault} 
+                className="img-responsive w-100"
+              />
+              <div className="card-body">
+                <h4 className="card-title">{name}</h4>
+                <p className="card-text">Continente: {continent}</p>
+                <div className="d-grid gap-2 mb-4 " key={country_id}>
+                  <Link href="/teams"> 
+                    <button type="button" className="btn btn-primary btn-lg btn-block">Equipos</button>
+                  </Link>
+                </div>
+              </div>
+              
+            </div>
+            
+          </div>
+           
+          )
         :
         <p>no data</p>
       }
