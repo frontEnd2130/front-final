c83c985d502e2a14e582216977d7adeadc518d28
bepalacio <30908023+bepalacio@users.noreply.github.com>
2021-11-13 19:53:55 -0500

    Botones de inicio

--- apps/front-final/pages/index.tsx
+++ apps/front-final/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './index.module.css';
 import Link from "next/link";
 import {Ui} from "@front-workspace/ui"
-import Countries from "./../app/countries/countries"
+import "bootstrap/dist/css/bootstrap.min.css"
 export function Index() {
   /*
    * Replace the elements below with your own.
@@ -10,9 +10,23 @@ export function Index() {
    */
   return (
     <div className={styles.page}>
-      <Ui title = "hola" showTitle/>
-      <Link href="/about"> About  </Link>
-      <Countries/>
+      <Ui title = "Sabana Futbol" showTitle/>
+      <div className="d-grid gap-2 mb-4">
+        <Link href="/countries"> 
+          <button type="button" className="btn btn-primary btn-lg btn-block">Paises</button>
+        </Link>
+      </div>
+      <div className="d-grid gap-2 mb-4 ">
+        <Link href="/teams"> 
+          <button type="button" className="btn btn-primary btn-lg btn-block">Equipos</button>
+        </Link>
+      </div>
+      <div className="d-grid gap-2 mb-4">
+        <Link href="/players"> 
+          <button type="button" className="btn btn-primary btn-lg btn-block">Jugadores</button>
+        </Link>
+      </div>
+      
     </div>
   );
 }
---
+++ apps/front-final/pages/teams.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import styles from './index.module.css';
+import Countries from "./../app/countries/countries"
+
+export function Team() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div className={styles.page}>
+      
+    </div>
+  );
+}
+
+export default Team;
